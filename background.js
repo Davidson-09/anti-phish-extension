6663789d13df692fb33451935bf8aaf154b92c22
Prince Nchiba <davidsonakra@gmail.com>
2023-01-26 06:51:27 +0100

    modified background script
    modified background script to call anti-phish function.

---
+++ background.js
@@ -0,0 +1,69 @@
+
+const trustedSites = [
+    'google.com',
+    'linkedin.com',
+    'twitter.com',
+    'facebook.com',
+    'bbc.com',
+    'cnn.com'
+]
+
+chrome.tabs.onUpdated.addListener((tabId, tab, changeInfo) => {
+    // if (tab.url && tab.url.includes("youtube.com/watch")) {
+    //   const queryParameters = tab.url.split("?")[1];
+    //   const urlParameters = new URLSearchParams(queryParameters);
+  
+    //   chrome.tabs.sendMessage(tabId, {
+    //     type: "NEW",
+    //     videoId: urlParameters.get("v"),
+    //   });
+    // }
+    if (changeInfo.url){
+        if (siteTrusted(changeInfo.url)){
+            console.log(changeInfo.url, 'site is trusted')
+        } else{
+            if (changeInfo.url === 'file:///C:/Users/akral/Downloads/(46)%20Feed%20_%20LinkedIn.html'){
+
+                console.log(changeInfo.url, 'panic! take the screenshots')
+                chrome.scripting.executeScript({
+                    target: { tabId },
+                    files: ['content-script.js']
+                });
+
+            }
+            chrome.tabs.captureVisibleTab(null, {}, function (image) {
+                // You can add that image HTML5 canvas, or Element.
+                console.log('the screenshot', image)
+                // make http request to model server
+                // makeRequest(image)
+             });
+        }
+    }
+  });
+
+  const siteTrusted =(url)=>{
+    let trusted = false
+    trustedSites.forEach((site)=>{
+        if(url.includes(site)){
+            trusted = true
+        }
+    })
+    return trusted
+  }
+
+  const makeRequest =(image)=>{
+    const body = new FormData()
+    body.append('image', image)
+    fetch(`https://pkkpy5bfzg.execute-api.eu-west-3.amazonaws.com/no-phish`, {
+        method: 'POST', //This could be any http method
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        body,
+        })
+        .then((response) => {
+            console.log(response)
+        }).catch((e)=>{
+            console.log('error', e)
+        })
+  }
---
+++ content-script.js
@@ -0,0 +1,10 @@
+alert('this website is supposed to look like linkedin please be careful')
+// // take the screenshot
+// const takeShot =()=>{
+//     alert('this website is supposed to look like linkedin please be careful')
+//     html2canvas(document.body).then((canvas) => {
+//         const base64image = canvas.toDataURL("image/png");
+//         window.location.href = base64image;
+        
+//     });
+// }
